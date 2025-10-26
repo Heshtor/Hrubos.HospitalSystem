8cff7c7a083457c7013fcdab9cc118c1856d8ceb
Heshtor <136596450+Heshtor@users.noreply.github.com>
2025-10-26 18:51:52 +0100

    Registrační a přihlašovací formulář s ViewModely

---
+++ Hrubos.HospitalSystem.Web/Areas/Security/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hrubos.HospitalSystem.Web.Areas.Security.Controllers
+{
+    [Area("Security")]
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
