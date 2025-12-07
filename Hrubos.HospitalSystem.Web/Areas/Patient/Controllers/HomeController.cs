df2ee54deb152395e5469cfed59dbafed5d4e0f5
Heshtor <136596450+Heshtor@users.noreply.github.com>
2025-12-07 18:21:44 +0100

    Stránka pro Patient panel

---
+++ Hrubos.HospitalSystem.Web/Areas/Patient/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Hrubos.HospitalSystem.Infrastructure.Identity.Enums;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hrubos.HospitalSystem.Web.Areas.Patient.Controllers
+{
+    [Area("Patient")]
+    [Authorize(Roles = nameof(Roles.Patient))]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
