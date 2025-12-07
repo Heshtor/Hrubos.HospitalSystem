6698f950743b8e89aaf8134d9fd0c0ff852dacd2
Heshtor <136596450+Heshtor@users.noreply.github.com>
2025-12-07 18:21:30 +0100

    Stránka pro Admin panel

---
+++ Hrubos.HospitalSystem.Web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Hrubos.HospitalSystem.Infrastructure.Identity.Enums;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hrubos.HospitalSystem.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = nameof(Roles.Admin))]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
